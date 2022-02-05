56bea1885f1027a0bbf79fdfb40ca1fdb7f209bf
melon <1067422237@qq.com>
2022-02-05 20:40:18 +0800

    master commit 04

--- src/main/java/com/dzh/controller/TestController.java
+++ src/main/java/com/dzh/controller/TestController.java
@@ -4,7 +4,6 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("git-demo01.....11111");
         System.out.println("这是master分支编写的代码11111111111");
-
-        System.out.println("这是hot-fix分支");
+        System.out.println("这是master分支编写的代码22222222");
     }
 }
